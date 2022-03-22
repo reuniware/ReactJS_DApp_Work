3df30f0c54e499cba23945322629b59610dd01ce
Quant & Fintech Opensource Projects <reunisoft@gmail.com>
2022-03-22 16:25:41 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -7,13 +7,33 @@ import { Text } from '@chakra-ui/react'
 import { Box } from '@chakra-ui/react'
 import GetDividendsButton from "./components/GetDividendsButton";
 import WithdrawButton from "./components/WithdrawButton";
+import { Image } from '@chakra-ui/react'
+import { Center, Square, Circle } from '@chakra-ui/react'
+import {useEffect} from "react";
+import AddPolygonNetworkButton from "./components/AddPolygonNetworkButton";
 
 
 export default function App() {
 
+    useEffect(() => {
+        // http://localhost:3000/?referrer=0x123
+        const queryParams = new URLSearchParams(window.location.search);
+        const referrer = queryParams.get('referrer');
+        if (referrer != null) {
+            alert("Referrer = [" + referrer + "]");
+        }
+    });
+
     return (
         <ChakraProvider>
+            <Center>
+                <Image src="/avaxroyalstaker.png" alt="avax royal staker"/>
+                <Text bgGradient='linear(to-l, #7928CA, #FF0080)' bgClip='text' fontSize='4xl' fontWeight='extrabold'>
+                    Welcome to Sakleotop Staker
+                </Text>
+            </Center>
             <Layout>
+                <Center><AddPolygonNetworkButton /*name='str'*/ /></Center>
                 <Text align='center' w='40%' bg='blackAlpha.500' color='gray.400' fontSize='4xl'>Supastaka</Text>
                 <Box h='5'/>
                 <ConnectButton />
